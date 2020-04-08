cb9c5afa806f04d0ead591632be3fa0b0d9addea
Solomon Ucko <solly.ucko@gmail.com>
2020-04-08 10:51:02 -0400

    Run rustfmt.

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ macro_rules! cfg_doc {
             @unbracket $decl {
                 unimplemented!("Documentation implementation!");
             }
-            
+
             $(
                 #[cfg(all(not(doc), $cfg))]
                 $(#[$attr])*
@@ -46,7 +46,7 @@ cfg_doc! {
     decl: [pub unsafe fn kill_thread<T>(handle: JoinHandle<T>)]
 
     /// ## Safety
-    /// 
+    ///
     /// Only kills the thread if it has enabled cancellation, then performs cleanup.
     /// See `man pthread_cancel` for more information.
     "# Unix" unix {
@@ -54,9 +54,9 @@ cfg_doc! {
     }
 
     /// Uses u32::MAX as the exit code.
-    /// 
+    ///
     /// ## Safety
-    /// 
+    ///
     /// Forcibly and immediately stops the thread, without any cleanup.
     /// See <https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminatethread>
     /// for more information.
@@ -69,18 +69,18 @@ cfg_doc! {
     /// Kills the thread, specifying the thread's exit code (if applicable).
 
     decl: [pub unsafe fn kill_thread_exit_code<T>(handle: JoinHandle<T>, exit_code: u32)]
-    
+
     /// ## Safety
-    /// 
+    ///
     /// Only kills the thread if it has enabled cancellation, then performs cleanup.
     /// See `man pthread_cancel` for more information.
     [allow(unused_variables)]
     "# Unix" unix {
         kill_thread_unix(handle);
     }
-   
+
     /// ## Safety
-    /// 
+    ///
     /// Forcibly and immediately stops the thread, without any cleanup.
     /// See <https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminatethread>
     /// for more information.
@@ -93,7 +93,7 @@ cfg_doc! {
     /// Kills the thread using `pthread_cancel`.
     ///
     /// # Safety
-    /// 
+    ///
     /// Only kills the thread if it has enabled cancellation, then performs cleanup.
     /// See `man pthread_cancel` for more information.
 
@@ -102,28 +102,28 @@ cfg_doc! {
     "# Unix" unix {
         use std::os::unix::thread::JoinHandleExt;
         use libc::pthread_cancel;
-             
+
         let raw_handle = handle.into_pthread_t();
         pthread_cancel(raw_handle);
     }
 }
 
 cfg_doc! {
-    /// Kills the thread using `TerminateThread`, specifying the thread's exit code. 
-    /// 
+    /// Kills the thread using `TerminateThread`, specifying the thread's exit code.
+    ///
     /// # Safety
-    /// 
+    ///
     /// Forcibly and immediately stops the thread, without any cleanup.
     /// See <https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminatethread>
     /// for more information.
-    
+
     decl: [pub unsafe fn kill_thread_windows<T>(handle: JoinHandle<T>, exit_code: u32)]
-    
+
     "# Windows" windows {
         use std::os::windows::io::IntoRawHandle;
         use winapi::um::processthreadsapi::TerminateThread;
         use winapi::ctypes::c_void as winapi_c_void;
-        
+
         let raw_handle = handle.into_raw_handle();
         TerminateThread(raw_handle as *mut winapi_c_void, exit_code);
     }
