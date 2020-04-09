9319dd79e01b5d967d7956a7088ae4590a7225a7
Solomon Ucko <solly.ucko@gmail.com>
2020-04-08 20:26:55 -0400

    Refactor stuff; add suspend_thread

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![feature(doc_cfg)]
 
+// clippy doesn't see the cfg(doc) version, and adding docs to the others would
+// likely lead to issues with macro_rules transcriber repeater nesting limitations
+#![allow(clippy::missing_safety_doc)]
+
 // #![feature(trace_macros)]
 // trace_macros!(true);
 
@@ -39,75 +43,58 @@ macro_rules! cfg_doc {
 }
 
 cfg_doc! {
-    /// Kills the thread using `pthread_cancel` or `TerminateThread`.
-
-    decl: [pub unsafe fn kill_thread<T>(handle: JoinHandle<T>)]
-
-    /// # Unix
-    ///
-    /// ## Safety
-    ///
     /// Only kills the thread if it has enabled cancellation, then performs cleanup.
     /// See `man pthread_cancel` for more information.
+    /// 
+    /// # Safety
+    ///
+    /// See `man pthread_cancel`.
+    
+    decl: [pub unsafe fn kill_thread_graceful<T>(handle: JoinHandle<T>)]
+    
     if unix {
-        #![allow(clippy::missing_safety_doc)]
-
         use std::os::unix::thread::JoinHandleExt;
         use libc::pthread_cancel;
 
         let raw_handle = handle.into_pthread_t();
         pthread_cancel(raw_handle);
     }
-
-    /// # Windows
-    ///
-    /// Uses u32::MAX as the exit code.
-    ///
-    /// ## Safety
-    ///
-    /// Forcibly and immediately stops the thread, without any cleanup.
-    /// See <https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminatethread>
-    /// for more information.
-    if windows {
-        #![allow(clippy::missing_safety_doc)]
-
-        kill_thread_exit_code(handle, u32::MAX);
-    }
 }
 
 cfg_doc! {
-    /// Kills the thread, specifying the thread's exit code (if applicable).
-
-    decl: [pub unsafe fn kill_thread_exit_code<T>(handle: JoinHandle<T>, exit_code: u32)]
-
-    /// # Unix
-    ///
-    /// ## Safety
-    ///
-    /// Only kills the thread if it has enabled cancellation, then performs cleanup.
-    /// See `man pthread_cancel` for more information.
-    if unix {
-        #![allow(unused_variables)]
-        #![allow(clippy::missing_safety_doc)]
-
-        kill_thread(handle);
-    }
-
-    /// # Windows
-    ///
-    /// ## Safety
-    ///
     /// Forcibly and immediately stops the thread, without any cleanup.
-    /// See <https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminatethread>
-    /// for more information.
+    /// 
+    /// # Safety
+    ///
+    /// See <https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminatethread>.
+    
+    decl: [pub unsafe fn kill_thread_forcibly_exit_code<T>(handle: JoinHandle<T>, exit_code: u32)]
+    
     if windows {
-        #![allow(clippy::missing_safety_doc)]
-
         use std::os::windows::io::IntoRawHandle;
         use winapi::um::processthreadsapi::TerminateThread;
         use winapi::ctypes::c_void as winapi_c_void;
 
         let raw_handle = handle.into_raw_handle();
         TerminateThread(raw_handle as *mut winapi_c_void, exit_code);
+    }
+}
+
+cfg_doc! {
+    /// Suspends the thread.
+    /// 
+    /// # Safety
+    /// 
+    /// See <https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-suspendthread>.
+    
+    decl: [pub unsafe fn suspend_thread<T>(handle: JoinHandle<T>)] 
+    
+    if windows {
+        use std::os::windows::io::IntoRawHandle;
+        use winapi::um::processthreadsapi::SuspendThread;
+        use winapi::ctypes::c_void as winapi_c_void;
+        
+        let raw_handle = handle.into_raw_handle();
+        SuspendThread(raw_handle as *mut winapi_c_void);
     }
 }
